bf7cff2142183b443f83166a960855e94a7e3e4a
ArpanSankesh <arpansankesh830@gmail.com>
2024-11-11 13:19:04 +0530

    Next page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header"
 import Page1 from "./pages/Page1"
+import Page2 from "./pages/Page2"
 
 const App = () => {
   return (
     <>
     <Header />
     <Page1 />
+    <Page2 />
     </>
   )
 }
