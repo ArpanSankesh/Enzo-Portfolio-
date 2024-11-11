70af83a28985fd24b7faffe245565eb6f2996aca
ArpanSankesh <arpansankesh830@gmail.com>
2024-11-11 10:01:31 +0530

    Landing Page

---
+++ src/components/TiltText.jsx
@@ -0,0 +1,13 @@
+
+
+const TiltText = () => {
+  return (
+    <div className="mt-40">
+        <h1 className="text-[4.5vw] leading-[4vw] font-[anzo1]">I AM <span className="text-black">DARK MODE</span>™</h1>
+        <h1 className="text-[7.5vw] leading-[6.5vw] font-[anzo1]">DESIGNER</h1>
+        <h1 className="text-[4.5vw] leading-[4vw] font-[anzo1]">TO HIRE</h1>
+    </div>
+  )
+}
+
+export default TiltText
