46ac4e9c8872aaf5560f8303e8e04cb7717e0cb6
ArpanSankesh <arpansankesh830@gmail.com>
2024-11-11 09:15:22 +0530

    Hero

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 
+import 'remixicon/fonts/remixicon.css'
 const Header = () => {
   return (
-    <div className="absolute text-white flex items-center justify-end w-full p-20">
-        <div className="bg-black px-10 py-3 rounded-full border-4 hover:bg-zinc-400 border-white">Hire me</div>
+    <div className=" fixed text-white flex items-center justify-end w-full px-5 py-20">
+        <div className="bg-black px-10 py-2 rounded-full border-4 hover:bg-zinc-400 border-white ">Hire me</div>
+        <i className="ri-more-2-line text-3xl ml-2"></i>
     </div>
   )
 }
