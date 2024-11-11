2e136dccd38ba0dd0205ac8232aabb6488f79e05
ArpanSankesh <arpansankesh830@gmail.com>
2024-11-11 18:54:59 +0530

    Next page

--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -27,8 +27,7 @@ const Page1 = () => {
   );
 
   return (
-    <div
-      onMouseMove={(e) => {
+    <div onMouseMove={(e) => {
         mouseMove(e);
       }} className="h-screen p-4 text-white">
       <div id="page-in" className="w-full h-full bg-[url(https://static.wixstatic.com/media/f1c650_678c0ceab5194893872c60fa3be4bcdc~mv2.jpg/v1/fill/w_2093,h_1196,fp_0.69_0.64,q_90,usm_0.66_1.00_0.01,enc_auto/ANZO.jpg)] bg-cover rounded-[2rem] shadow-lg shadow-grey-700 px-14 py-12">
